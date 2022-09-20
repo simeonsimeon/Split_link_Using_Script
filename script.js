da2464212cecd9af7676ba2964932dd490ea7e91
simeonsimeon <lingakumardae@gmail.com>
2022-09-19 20:59:30 -0600

    add

---
+++ script.js
@@ -0,0 +1,51 @@
+// let link="http://WWW.youtube.com";
+// let split=link.split(":");
+// let second=link.split("/");
+// let third=link.split(".");
+// console.log(third);
+// let first=split[0];
+// console.log(first);
+// console.log(second[2]);
+// let then=third[1];
+// let then2=third[2];
+// let se=second[2].split(".");
+// let rol=se[0];
+// console.log(rol);
+// let grand=first+rol+then+then2;
+// console.log(grand);
+
+  
+
+//  var input=document.getElementById("check");
+//  let div=document.getElementById("div");
+// var person={
+//      lenin:{ name:"lenin", number:9095,  character:"@"},
+//     susa:{name:"susa",  number:9952,   character:"$"},
+// 	selvam:{name:"selvan", number:8667,  character:"#"}
+// }
+// function easy(){
+// if(input.value=="lenin"){	 
+// 			for(let key in person.lenin){
+                
+//                  let li=document.createElement("li");
+//                  li.innerText=person.lenin[key];
+//                  div.appendChild(li)
+//                 }
+//             }else if(input.value=="susa"){
+//                 for(let key in person.susa){
+                
+//                     let li=document.createElement("li");
+//                     li.innerText=person.susa[key];
+//                     div.appendChild(li)
+//                    }
+//             }else{
+//                 for(let key in person.susa){
+                
+//                     let li=document.createElement("li");
+//                     li.innerText=person.selvan[key];
+//                     div.appendChild(li)
+//                    }
+//             }
+//             input.value="";
+//         }
+        
